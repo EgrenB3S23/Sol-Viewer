eeac43377cc12a448c2f77a5256f5851fa151909
Erik Hallgren <erikhallgren@live.se>
2024-08-16 21:59:18 +0200

    added settings and basic planet/moon info on click.

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function createPlanetElements() {
 			background-color: ${planetColors[i]}`;
 
 			addHoverListenerFor(newPlanet);
-
+			addClickListenerFor(newPlanet);
 			// add newly created HTML tag to document
 			sunHTML.appendChild(newPlanet);
 
@@ -211,6 +211,8 @@ function createMoonElements(planetId = null) {
 			--distance-moon: ${myMoons[i].semimajorAxis};
 			`;
 			addHoverListenerFor(newMoon);
+			addClickListenerFor(newMoon);
+
 			// add newly created HTML tag to document
 			planetHTML.appendChild(newMoon);
 			console.log(`Added moon "${myMoons[i].id}"`);
@@ -224,10 +226,7 @@ function getScaleFactor() {
 	// fetches the value of the inline css "transform: scale(#)".
 	// without this, the first zoom event always starts out from scale 1, potentially causing a massive jump.
 	let scaleFactorStr = elem.style.transform; // -> scaleFactor example value: "scale(1)"
-	// let scaleFactorStr2 = Number(document.querySelector(".zoomable").style.scale);
 	console.log("scaleFactorStr:", scaleFactorStr, typeof scaleFactorStr);
-	// console.log("scaleFactorStr2:", scaleFactorStr2, typeof scaleFactorStr2);
-	// let scaleFactor = Number(document.querySelector(".zoomable").style.scale);
 
 	let startIndex = 6; // -> scaleFactor example value: "1)"
 	let endIndex = scaleFactorStr.length - 1; // -> scaleFactor example value: "1"
@@ -239,14 +238,6 @@ function getScaleFactor() {
 	} else {
 		return 1;
 	}
-
-	/*
-	if (typeof scaleFactorStr2 === "number" && scaleFactorStr2 > 0) {
-		return scaleFactorStr2;
-	} else {
-		return 1;
-	}
-	*/
 }
 
 function zoom() {
@@ -319,16 +310,76 @@ function dragElement(elmnt) {
 	}
 }
 
-function addHoverListenerFor(celestial) {
-	// celestial.addEventListener("click", () => {
+function addClickListenerFor(celestialHTML) {
+	console.log("adding clicl listener for: ", celestialHTML.id);
+
+	celestialHTML.addEventListener(
+		"click",
+		() => {
+			const menuItemNameHTML = document.getElementById("selectedCelestialName");
+			const menuItemYearLengthHTML = document.getElementById("selectedCelestialYearLength");
+			const menuItemDayLengthHTML = document.getElementById("selectedCelestialDayLength");
+			const menuItemSatelitesHTML = document.getElementById("selectedCelestialSatelites");
+
+			/* if planet, do */
+
+			// determine which localStorage entry to fetch data from:
+			if (celestialHTML.classList.contains("planet")) {
+				celestialType = "planet";
+			} else if (celestialHTML.classList.contains("moon")) {
+				celestialType = "moon";
+			}
+			console.log(celestialType);
+			const celestialsList = JSON.parse(localStorage.getItem(celestialType + "s")); // if class is "planet", the localStorage entry is "planets".
+			// console.log(celestialsList);
+			console.log("celestialHTML.id", celestialHTML.id);
+
+			const celestialsListFiltered = celestialsList.filter((entry) => entry.id === celestialHTML.id);
+			const singleCelestial = celestialsListFiltered[0]; //pick the first and presumably only entry in the array (which should be all data for the given celestial)
+
+			menuItemNameHTML.innerText = singleCelestial.englishName;
+			menuItemYearLengthHTML.innerText = singleCelestial.sideralOrbit;
+			menuItemDayLengthHTML.innerText = singleCelestial.sideralRotation;
+
+			// extract a list of satelite names from the list of satelite objects
+			const satelitesList = singleCelestial.moons;
+			let sateliteNames = [];
+			if (satelitesList !== null) {
+				for (let i = 0; i < satelitesList.length; i++) {
+					sateliteNames.push(satelitesList[i].moon);
+				}
+			}
+			menuItemSatelitesHTML.innerText = sateliteNames.join(" | ") || "(None)";
+
+			console.log("opening menu...");
+
+			const menu = document.getElementById("menu");
+			console.log(menu);
+
+			// menu.classList.toggle("open");
+			// menu.classList.remove("open");
+			menu.classList.add("open");
+			console.log(menu);
+
+			document.getElementById("selectedCelestialInfo").classList.remove("hide");
+			document.getElementById("tipClickACelestial").classList.add("hide");
+
+			// meanRadius
+		},
+		true
+	); // true: event propagation set to "capturing" instead of the default "bubbling"
+}
+
+function addHoverListenerFor(celestialHTML) {
+	// celestialHTML.addEventListener("click", () => {
 	// 	window.location.href = `singlePlanet.html?planet=${planetName.toLowerCase()}`;
 	// });
 
-	celestial.addEventListener("mouseenter", () => {
-		document.querySelector("#nameHeader").textContent = celestial.id; // Visa planetens namn vid hover
-		document.querySelector("#nameHeader").textContent = celestial.id; // Visa planetens namn vid hover
+	celestialHTML.addEventListener("mouseenter", () => {
+		document.querySelector("#nameHeader").textContent = celestialHTML.id; // Visa planetens namn vid hover
+		document.querySelector("#nameHeader").textContent = celestialHTML.id; // Visa planetens namn vid hover
 	});
-	celestial.addEventListener("mouseleave", () => {
+	celestialHTML.addEventListener("mouseleave", () => {
 		document.querySelector("#nameHeader").textContent = "Sol-viewer"; // Dölj planetens namn när muspekaren lämnar
 	});
 }
--- menu.js
+++ menu.js
@@ -1,17 +1,48 @@
-// document.getElementById("menuToggle").addEventListener("click", () => {
-// 	const menuElem = document.getElementById("menu");
-// 	menuElem.classList.toggle("open");
-// 	// window.location.href = `singlePlanet.html?planet=${planetName.toLowerCase()}`;
-// });
+// menu open toggle listener
 document.querySelector(".toggle").addEventListener("click", (e) => {
-	document.querySelector("nav").classList.toggle("open");
+	document.getElementById("menu").classList.toggle("open");
 });
 
-document.addEventListener("click", (e) => {
-	const nav = document.querySelector("nav");
-	const toggler = document.querySelector(".toggle");
-	if (!nav.contains(e.target) && !toggler.contains(e.target)) {
-		// alert("!!!");
-		nav.classList.remove("open");
+// animation toggle listener
+document.getElementById("checkboxAnimate").addEventListener("change", (e) => {
+	//toggle animation on sun (if there was any)
+	document.getElementById("soleil").classList.toggle("pause");
+});
+
+// orbit ring toggle listener
+document.getElementById("checkboxOrbitRings").addEventListener("change", (e) => {
+	// toggle orbit ring on planets
+	planets = document.getElementsByClassName("planet");
+	for (const planet of planets) {
+		planet.classList.toggle("hide-orbit-rings");
+	}
+	// toggle orbit ring on moons
+	moons = document.getElementsByClassName("moon");
+	for (const moon of moons) {
+		moon.classList.toggle("hide-orbit-rings");
 	}
 });
+
+// highlight ring toggle listener
+document.getElementById("checkboxHighlightRings").addEventListener("change", (e) => {
+	// toggle highlight ring on planets
+	planets = document.getElementsByClassName("planet");
+	for (const planet of planets) {
+		planet.classList.toggle("hide-highlight-rings");
+	}
+	// toggle highlight ring on moons
+	moons = document.getElementsByClassName("moon");
+	for (const moon of moons) {
+		moon.classList.toggle("hide-highlight-rings");
+	}
+});
+
+// if clicking outside of menu, close menu
+
+// document.addEventListener("click", (e) => {
+// 	const menu = document.getElementById("menu");
+// 	const toggler = document.querySelector(".toggle");
+// 	if (!menu.contains(e.target) && !toggler.contains(e.target)) {
+// 		menu.classList.remove("open");
+// 	}
+// });
